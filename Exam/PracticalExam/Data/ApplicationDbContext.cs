b8dc4a03ededd3f4bcaf9f4f700056441f51102c
nam <daothanhnam0923@gmail.com>
2022-12-17 20:29:31 +0700

    Exam :+1:

---
+++ Exam/PracticalExam/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticalExam.Models;
+
+namespace PracticalExam.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Exam/PracticalExam/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticalExam.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Department { get; set; }
+        [Required]
+        public int Salary { get; set; }
+    }
+}
